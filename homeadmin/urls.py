9b01da6210562524ad754aeaee9d334eef9798d7
Erik Ulven <erik@kulturit.no>
2013-05-17 15:35:38 +0200

    fixed static dir...

--- homeadmin/urls.py
+++ homeadmin/urls.py
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 
-if not settings.DEBUG:
+if settings.DEBUG:
     urlpatterns += patterns('',
         (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     )
